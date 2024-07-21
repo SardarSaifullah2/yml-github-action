7cf8e8dab33a28121d7c9a312af3f107d7a8132b
Sardar Saifullah <saifisardar5656@yahoo.com>
2024-07-20 22:22:38 -0700

    setting env

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { environment } from '../environments/environment';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -11,5 +11,9 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'yml-github-action';
   // console.log(process.env.SECRET_KEY);
-  
+    private apiUrl = environment.apiUrl;  // Access the API URL from the environment
+
+  constructor() {
+    console.log('API URL:', this.apiUrl);  // Print the API URL to the console
+  }
 }
